1d56def17e08ac1ebe3df1089fdaf0bd18bad07d
Irving <dev.lemirving@gmail.com>
2025-05-23 13:12:17 -0500

    Primeiro commit

---
+++ src/main/java/com/project/project_healtheducation/model/Aluno.java
@@ -0,0 +1,4 @@
+package com.project.project_healtheducation.model;
+
+public class Aluno {
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module com.project.project_healtheducation {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.project.project_healtheducation to javafx.fxml;
+    exports com.project.project_healtheducation;
+    exports com.project.project_healtheducation.controllers;
+    opens com.project.project_healtheducation.controllers to javafx.fxml;
+}
