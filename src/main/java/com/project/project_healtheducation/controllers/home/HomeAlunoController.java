90933aa84f9628c1a6a4479925857f718779fefe
CarlosEduDev <carloseduardoevang@gmail.com>
2025-06-25 21:22:21 -0400

    feat: alteração na tela paginaInicial

--- src/main/java/com/project/project_healtheducation/controllers/home/HomeAlunoController.java
+++ src/main/java/com/project/project_healtheducation/controllers/home/HomeAlunoController.java
@@ -1,8 +1,11 @@
 package com.project.project_healtheducation.controllers.home;
 
+import com.project.project_healtheducation.model.Usuario;
 import com.project.project_healtheducation.utils.ChangeScreen;
+import com.project.project_healtheducation.utils.SessaoUsuario;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
@@ -11,6 +14,17 @@ public class HomeAlunoController {
 
     @FXML
     private AnchorPane main_anchorPane;
+    @FXML private Label labelSaudacao;
+
+    @FXML
+    private void initialize(){
+        Usuario usuario = SessaoUsuario.getUsuarioLogado();
+        if (usuario != null) {
+            labelSaudacao.setText("Olá, " + usuario.getNome() + "! Como você se sente hoje?");
+        } else {
+            labelSaudacao.setText("Olá! Como você se sente hoje?");
+        }
+    }
 
     @FXML
     private void handleTelaPerfil(ActionEvent event) {
